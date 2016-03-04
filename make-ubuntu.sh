e59098c2cbf9d4118ef3d331f49c2acff8cb1621
Chloride Cull <chloride@devurandom.net>
2016-03-04 15:34:52 +0100

    Separate into multiple packages

--- make-ubuntu.sh
+++ make-ubuntu.sh
@@ -1,4 +1,32 @@
 #!/bin/sh
+
+build-deb() {
+	echo "--> building .deb"
+	tmpdir=$(mktemp -d)
+	cd "$4"
+	tar --owner=root --group=root -cf $tmpdir/data.tar.gz .
+	cd "$tmpdir"
+	echo "2.0" > debian-binary
+	mkdir control && cd control
+	tee control << EOF
+Package: ${2}
+Desc: ${2} from Arch Linux.
+Version: ${3}
+Architecture: amd64
+Maintainer: Chloride Cull <steamruler@gmail.com>
+${5}
+Origin: archlinux
+EOF
+	tar --owner=root --group=root -cf ../control.tar control
+	cd ..
+	ar cq ${1}/${2}-${3}.deb debian-binary control.tar data.tar.gz
+	echo "--> a simple package is available at"
+	echo "-->    ${1}/${2}-${3}.deb"
+	echo "--> it contains no scripts."
+	cd "$4"
+	rm -rf "$tmpdir"
+}
+
 echo "--> preparing build on ubuntu (assuming x86_64)"
 echo "--> installing script prereqs"
 sudo apt-get update
@@ -8,28 +36,22 @@ sudo apt-get --no-install-recommends install bash libc6-dev libarchive-dev libgp
 echo "--> installing basic compiling and packaging environment"
 sudo apt-get --no-install-recommends install build-essential pkg-config
 MACHINEREADABLE=y ./make.sh | tee make.log
-echo "--> building .deb"
-bindir=$(grep "PKGDIR: " make.log | sed 's/PKGDIR: //g')
-pkgver=$(grep "PKGVER: " make.log | sed 's/PKGVER: //g')
-pkgdir=$(mktemp -d)
-cd "$bindir"
-tar --owner=root --group=root -cf $pkgdir/data.tar.gz .
-cd "$pkgdir"
-echo "2.0" > debian-binary
-mkdir control && cd control
-tee control << EOF
-Package: pacman
-Version: ${pkgver}
-Architecture: amd64
-Maintainer: Chloride Cull <steamruler@gmail.com>
-Depends: bash, libc6, libarchive13, curl, libgpgme11
-Original-Maintainer: Allan McRae <allan@archlinux.org>
-Origin: archlinux
-EOF
-tar --owner=root --group=root -cf ../control.tar control
-cd ..
-ar cq pacman-${pkgver}.deb debian-binary control.tar data.tar.gz
-echo "--> a simple package is available at"
-echo "-->    ${pkgdir}/pacman-${pkgver}.deb"
-echo "--> it contains no scripts."
+makelog=$(cat make.log)
+outdir=$(mktemp -d)
+
+build-deb $outdir \
+	"pacman" \
+	$(echo "$makelog" | grep "PACMAN-PKGVER: " | sed 's/PACMAN-PKGVER: //g') \
+	$(echo "$makelog" | grep "PACMAN-PKGDIR: " | sed 's/PACMAN-PKGDIR: //g') \
+	"Depends: bash, libc6, libarchive13, libcurl4, libgpgme11, libssl, pacman-archlinux-mirrorlist"
+build-deb $outdir \
+	"pacman-archlinux-mirrorlist" \
+	$(echo "$makelog" | grep "MLIST-PKGVER: " | sed 's/MLIST-PKGVER: //g') \
+	$(echo "$makelog" | grep "MLIST-PKGDIR: " | sed 's/MLIST-PKGDIR: //g') \
+	"Depends: pacman-archlinux-keyring"
+build-deb $outdir \
+	"pacman-archlinux-keyring"
+	$(echo "$makelog" | grep "KEYRING-PKGVER: " | sed 's/KEYRING-PKGVER: //g') \
+	$(echo "$makelog" | grep "KEYRING-PKGDIR: " | sed 's/KEYRING-PKGDIR: //g') \
+	"# No Depends"
 
--- make.sh
+++ make.sh
@@ -23,12 +23,13 @@ dl_and_check_sig() {
 }
 
 echo "--> Building pacman version ${pkgver} in ${tmpdir}."
-echo "--> Binaries will be placed in ${pkgdir}."
+echo "--> Binaries will be placed in subdirectories of ${pkgdir}."
 if [ -n "$MACHINEREADABLE" ]; then
-	echo "PKGDIR: ${pkgdir}"
-	echo "PKGVER: ${pkgver}"
+	echo "PACMAN-PKGDIR: ${pkgdir}/pacman"
+	echo "PACMAN-PKGVER: ${pkgver}"
 fi
-mkdir ${pkgdir}
+mkdir "${pkgdir}"
+mkdir "$pkgdir/pacman"
 cd $tmpdir
 add_valid_sig pacman 6645B0A8C7005E78DB1D7864F99FFE0FEAE999BD # Signed by Allan
 dl_and_check_sig pacman ${pkgver}
@@ -40,26 +41,30 @@ cd pacman-${pkgver}
             --with-ldconfig=/bin/ldconfig
 make V=1
 make -C contrib
-make DESTDIR="${pkgdir}" install
-make DESTDIR="${pkgdir}" -C contrib install
+make DESTDIR="$pkgdir/pacman" install
+make DESTDIR="$pkgdir/pacman" -C contrib install
 echo "--> pacman version ${pkgver} has been built."
-if [ -n "$NOARCHLINUX" ]; then
-	echo "--> not creating arch linux specifics."
-	echo "--> done!"
-	exit 0
-fi
 echo "--> creating arch linux mirrorlist."
-mkdir -p "${pkgdir}/etc/pacman.d"
-curl 'https://www.archlinux.org/mirrorlist/all/?country=all&protocol=http&protocol=https&ip_version=6' | sed 's/^\#Server/Server/g' > "${pkgdir}/etc/pacman.d/mirrorlist"
+if [ -n "$MACHINEREADABLE" ]; then
+	echo "MLIST-PKGDIR: ${pkgdir}/pacman-archlinux-mirrorlist"
+	echo "MLIST-PKGVER: $(date +%Y%m%d)"
+fi
+mkdir -p "${pkgdir}/pacman-archlinux-mirrorlist/etc/pacman.d"
+curl 'https://www.archlinux.org/mirrorlist/all/?country=all&protocol=http&protocol=https&ip_version=6' | sed 's/^\#Server/Server/g' > "${pkgdir}pacman-archlinux-mirrorlist/etc/pacman.d/mirrorlist"
 echo "--> fetching arch linux keyring."
 keyringver=20160215
+mkdir "$pkgdir/pacman-archlinux-keyring"
+if [ -n "$MACHINEREADABLE" ]; then
+	echo "KEYRING-PKGDIR: ${pkgdir}/pacman-archlinux-keyring"
+	echo "KEYRING-PKGVER: ${keyringver}"
+fi
 add_valid_sig archlinux-keyring 4AA4767BBC9C4B1D18AE28B77F2D434B9741E8AC # Pierre
 add_valid_sig archlinux-keyring A314827C4E4250A204CE6E13284FC34C8E4B1A25 # Thomas
 add_valid_sig archlinux-keyring 86CFFCA918CF3AF47147588051E8B148A9999C34 # Evangelos
 dl_and_check_sig archlinux-keyring $keyringver
 cd archlinux-keyring-${keyringver}
-make PREFIX=/usr DESTDIR="${pkgdir}" install
-cd "$pkgdir"
+make PREFIX=/usr DESTDIR="${pkgdir}/pacman-archlinux-keyring" install
+cd "$pkgdir/pacman"
 echo "--> patching pacman.conf"
 echo "$pacmanpatch" | patch "${pkgdir}/etc/pacman.conf"
 echo "--> done!"
